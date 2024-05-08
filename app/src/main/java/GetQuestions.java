777f4d85f56ec72db903e1d970e98a7fdeddcb2a
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2024-05-08 14:40:57 -0400

    update

---
+++ app/src/main/java/GetQuestions.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GetQuestions {
+    public ArrayList<TriviaQuestion> questions;
+
+    public GetQuestions() {
+        try {
+            questions = new ArrayList<TriviaQuestion>();
+
+            Scanner inFile = new Scanner(new FileReader("sports.txt"));
+
+            while (inFile.hasNext()) {
+                String question = inFile.nextLine();
+                String choice1 = inFile.nextLine();
+                String choice2 = inFile.nextLine();
+                String choice3 = inFile.nextLine();
+                String choice4 = inFile.nextLine();
+                int answer = Integer.parseInt(inFile.nextLine());
+                TriviaQuestion tq = new TriviaQuestion(question, choice1, choice2, choice3, choice4, answer);
+                questions.add(tq);
+            }
+
+        } catch (java.io.FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    public static void main(String[] args) {
+        GetQuestions gq = new GetQuestions();
+        gq.questions.forEach(q -> {
+            System.out.println(q.getQuestion());
+            System.out.println(q.getOption(0));
+            System.out.println(q.getOption(1));
+            System.out.println(q.getOption(2));
+            System.out.println(q.getOption(3));
+            System.out.println(q.getCorrectAnswerIndex());
+        });
+    }
+
+
+}
